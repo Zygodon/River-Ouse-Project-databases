ec7d5930d74fe05442cedf886b7877d50c72e154
zygodon <j.b.pilkington@gmail.com>
2019-12-02 19:18:16 +0000

    Using SpeciesFrequencyWithStandard in global.R

--- global.R
+++ global.R
@@ -3,5 +3,23 @@ source("db_extract.R")
 # Reduced version of the_data excluding Mires, i.e. only mesotrophic grassland
 the_data <- GetTheData() %>% filter(grepl("MG", community))
 
-q <- "SELECT community, species_id, p_central FROM meadows.mg_rodwell where community like 'MG%';"
-std_freqs <- query(q)
+# q <- "SELECT community, species_id, p_central FROM meadows.mg_rodwell where community like 'MG%';"
+# std_freqs <- query(q)
+
+SpeciesFreqWithStandard <- function(t_d, spn)
+{
+  q <- "SELECT community, species_id, p_central FROM meadows.mg_rodwell where community like 'MG%';"
+  std_freqs <- query(q)
+  
+  q <- paste('SELECT species_name, species_id FROM meadows.species where species_name = "',spn, '";', sep = "")  
+  s <- query(q)
+  stdf <- std_freqs %>% filter(species_id == s$species_id)
+  d <- (FrequencyByAssembly(t_d) 
+        %>% filter(species_name == spn) 
+        %>% select(assembly_id, assembly_name, community, freq, CrI5, median, CrI95))
+  d1 <- left_join(d, stdf, by = "community")
+  d2 <- replace_na(d1, list(p_central=0)) %>% select(-species_id)
+
+  return(d2)
+}
+  
